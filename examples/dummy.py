211691c2d916b7b6f348bd626b19bb3027befa47
d-k-bo <47948262+d-k-bo@users.noreply.github.com>
2022-04-26 15:22:48 +0200

    Provide simple local example

---
+++ examples/dummy.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from aiotinydb import AIOTinyDB
+
+async def do_something():
+    print("Starting slow IO bound operation...")
+    await asyncio.sleep(1)
+    print("Completed IO bound operation.")
+
+    return {"key": "value"}
+
+async def main():
+    print("Using 'db.json' as database.")
+    async with AIOTinyDB("db.json") as db:
+        document = await do_something()
+        db.insert(document)
+    print("Wrote database as 'db.json' to disk.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
